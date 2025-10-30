21baabd838230a4cd886c5f632c5d7ea303703a0
Nicolas Voges <nicolas.voges@hotmail.com>
2025-10-30 12:23:15 +0100

    Test: Complete registration tests.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -17,7 +17,7 @@ class AuthTests(APITestCase):
             'password': "securepassword",
             'confirmed_password': "securepassword"
         }
-        
+
     @override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend")
     def test_register_success(self):
         response = self.client.post(self.register_url, self.register_data, format='json')
@@ -27,3 +27,27 @@ class AuthTests(APITestCase):
         self.assertEqual(len(mail.outbox), 1)
         self.assertIn("Confirm your email", mail.outbox[0].subject)
         self.assertIn(self.user_mail, mail.outbox[0].to)
+
+    
+    @override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend")
+    def test_register_fails(self):
+        data_invalid_password = {
+            'email': "test@test.de",
+            'password': "securepassword",
+            'confirmed_password': "wrong"
+        }
+        data_invalid_email = {
+            'email': self.user_mail,
+            'password': "securepassword",
+            'confirmed_password': "securepassword"
+        }
+        # First, register the user successfully
+        self.client.post(self.register_url, self.register_data, format='json')
+        cases = [
+            (data_invalid_password, status.HTTP_400_BAD_REQUEST),
+            (data_invalid_email, status.HTTP_400_BAD_REQUEST),
+        ]
+
+        for data, expected_status in cases:
+            response = self.client.post(self.register_url, data, format='json')
+            self.assertEqual(response.status_code, expected_status)
