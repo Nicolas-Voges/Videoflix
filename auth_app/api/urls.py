9b390ae1d37c0be7d2255ad17e662bcd6c392e76
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-11 23:12:41 +0100

    Feat: Add activation URL.

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import RegisterView, ActivationView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
+    path('activate/<str:uidb64>/<str:token>/', ActivationView.as_view(), name="activate"),
 ]
