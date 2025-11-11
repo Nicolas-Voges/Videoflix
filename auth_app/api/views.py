dd31a03284c7794144f8641a4f80a725747b7fdf
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-11 21:47:36 +0100

    Feat: Add ActivationView.

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,7 +1,30 @@
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth import get_user_model
+
+from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
-from auth_app.api.serializers import RegisterSerializer
+from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
+
+from auth_app.api.serializers import RegisterSerializer
 
 class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
+
+User = get_user_model()
+class ActivationView(APIView):
+    def get(self, request, uidb64, token):
+        try:
+            uid = urlsafe_base64_decode(uidb64).decode()
+            user = User.objects.get(pk=uid)
+        except (User.DoesNotExist, ValueError, TypeError, OverflowError):
+            return Response({"error": "Invalid link"}, status=400)
+
+        if default_token_generator.check_token(user, token):
+            user.is_active = True
+            user.save()
+            return Response({"success": "Account activated"}, status=200)
+        else:
+            return Response({"error": "Invalid or expired token"}, status=400)
