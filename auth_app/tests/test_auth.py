f389fc5cee04cf919a446d204a054488b7a365ed
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-11 16:47:52 +0100

    Test: Add activation tests.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -1,7 +1,11 @@
+import time
+
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test import override_settings
 from django.core import mail
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
 
 from rest_framework.test import APITestCase
 from rest_framework import status
@@ -12,6 +16,7 @@ class AuthTests(APITestCase):
     def setUp(self):
         self.user_mail = "user@example.com"
         self.register_url = reverse('register')
+        self.activision_url = reverse('activate')
         self.register_data = {
             'username': 'TestUser',
             'email': self.user_mail,
@@ -53,3 +58,71 @@ class AuthTests(APITestCase):
         for data, expected_status in cases:
             response = self.client.post(self.register_url, data, format='json')
             self.assertEqual(response.status_code, expected_status)
+
+    
+    def test_activate_success(self):
+        user = User.objects.create_user(
+            username="TestUser",
+            password="Test123$",
+            email=self.user_mail,
+            is_active=False
+        )
+        uidb64 = urlsafe_base64_encode(str(user.pk).encode('utf-8'))
+        token = default_token_generator.make_token(user)
+        respons = self.client.get(
+            self.activision_url,
+            kwargs={'uidb64': uidb64, 'token': token}
+        )
+
+        user.refresh_from_db()
+
+        self.assertEqual(respons.status_code, status.HTTP_200_OK)
+        self.assertTrue(user.is_active)
+
+
+    def test_activate_invalid_token(self):
+        user = User.objects.create_user(
+            username="TestUser",
+            password="Test123$",
+            email=self.user_mail,
+            is_active=False
+        )
+
+        uidb64 = urlsafe_base64_encode(str(user.pk).encode('utf-8'))
+        token = default_token_generator.make_token(user)
+        invalid_token = token + "x"
+
+        response = self.client.get(
+            self.activision_url,
+            kwargs={"uidb64": uidb64, "token": invalid_token}
+        )
+
+        user.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse(user.is_active)
+
+
+    @override_settings(PASSWORD_RESET_TIMEOUT=1)
+    def test_activate_expired_token(self):
+        user = User.objects.create_user(
+            username="TestUser",
+            password="Test123$",
+            email=self.user_mail,
+            is_active=False
+        )
+
+        uidb64 = urlsafe_base64_encode(str(user.pk).encode('utf-8'))
+        token = default_token_generator.make_token(user)
+
+        time.sleep(2)
+
+        response = self.client.get(
+            self.activision_url,
+            kwargs={"uidb64": uidb64, "token": token}
+        )
+
+        user.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertFalse(user.is_active)
