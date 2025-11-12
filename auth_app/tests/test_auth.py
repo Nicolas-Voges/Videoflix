315c9472f1c6404495171353accd138bd01cf6ed
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-13 00:48:03 +0100

    Test: Refactor test_auth.py.

--- auth_app/tests/test_auth.py
+++ auth_app/tests/test_auth.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 from django.test import override_settings
 from django.core import mail
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 
 from rest_framework.test import APITestCase
@@ -12,100 +12,94 @@ from rest_framework import status
 
 User = get_user_model()
 
-class AuthTests(APITestCase):
+class RegisterTests(APITestCase):
     def setUp(self):
-        self.user_mail = "user@example.com"
-        self.register_url = reverse('register')
-        self.login_url = reverse('login')
-        self.register_data = {
+        self.user_email = "user@example.com"
+        self.url = reverse('register')
+        self.data = {
             'username': 'TestUser',
-            'email': self.user_mail,
+            'email': self.user_email,
             'password': "securepassword",
             'confirmed_password': "securepassword"
         }
 
+
     @override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend")
-    def test_register_success(self):
-        response = self.client.post(self.register_url, self.register_data, format='json')
+    def test_post_success(self):
+        response = self.client.post(self.url, self.data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(User.objects.filter(email=self.user_mail).exists())
-        self.assertFalse(User.objects.get(email=self.user_mail).is_active)
+        self.assertTrue(User.objects.filter(email=self.user_email).exists())
+        self.assertFalse(User.objects.get(email=self.user_email).is_active)
         self.assertEqual(len(mail.outbox), 1)
         self.assertIn("Confirm your email", mail.outbox[0].subject)
-        self.assertIn(self.user_mail, mail.outbox[0].to)
+        self.assertIn(self.user_email, mail.outbox[0].to)
 
     
     @override_settings(EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend")
-    def test_register_fails(self):
+    def test_post_fails(self):
         data_invalid_password = {
             'email': "test@test.de",
             'password': "securepassword",
             'confirmed_password': "wrong"
         }
         data_invalid_email = {
-            'email': self.user_mail,
+            'email': self.user_email,
             'password': "securepassword",
             'confirmed_password': "securepassword"
         }
         # First, register the user successfully
-        self.client.post(self.register_url, self.register_data, format='json')
+        self.client.post(self.url, self.data, format='json')
         cases = [
             (data_invalid_password, status.HTTP_400_BAD_REQUEST),
             (data_invalid_email, status.HTTP_400_BAD_REQUEST),
         ]
 
         for data, expected_status in cases:
-            response = self.client.post(self.register_url, data, format='json')
+            response = self.client.post(self.url, data, format='json')
             self.assertEqual(response.status_code, expected_status)
 
     
-    def test_activate_success(self):
-        user = User.objects.create_user(
+class AccountActivationTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
             username="TestUser",
             password="Test123$",
-            email=self.user_mail,
+            email='test@test.de',
             is_active=False
         )
-        uidb64 = urlsafe_base64_encode(str(user.pk).encode('utf-8'))
-        token = default_token_generator.make_token(user)
-        response = self.client.get(reverse('activate', kwargs={"uidb64": uidb64, "token": token}))
+        self.uidb64 = urlsafe_base64_encode(str(self.user.pk).encode('utf-8'))
+        self.token = default_token_generator.make_token(self.user)
 
-        user.refresh_from_db()
+
+    def test_get_success(self):
+        response = self.client.get(reverse('activate', kwargs={"uidb64": self.uidb64, "token": self.token}))
+
+        self.user.refresh_from_db()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(user.is_active)
+        self.assertTrue(self.user.is_active)
 
 
-    def test_activate_invalid_token(self):
-        user = User.objects.create_user(
-            username="TestUser",
-            password="Test123$",
-            email=self.user_mail,
-            is_active=False
-        )
+    def test_get_invalid_token(self):
+        invalid_token = self.token + "x"
 
-        uidb64 = urlsafe_base64_encode(str(user.pk).encode('utf-8'))
-        token = default_token_generator.make_token(user)
-        invalid_token = token + "x"
+        response = self.client.get(reverse('activate', kwargs={"uidb64": self.uidb64, "token": invalid_token}))
 
-        response = self.client.get(reverse('activate', kwargs={"uidb64": uidb64, "token": invalid_token}))
-
-        user.refresh_from_db()
+        self.user.refresh_from_db()
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertFalse(user.is_active)
+        self.assertFalse(self.user.is_active)
 
 
     @override_settings(PASSWORD_RESET_TIMEOUT=1)
-    def test_activate_expired_token(self):
+    def test_get_expired_token(self):
         user = User.objects.create_user(
-            username="TestUser",
+            username="NewUser",
             password="Test123$",
-            email=self.user_mail,
+            email='new@new.de',
             is_active=False
         )
-
         uidb64 = urlsafe_base64_encode(str(user.pk).encode('utf-8'))
         token = default_token_generator.make_token(user)
 
@@ -119,17 +113,38 @@ class AuthTests(APITestCase):
         self.assertFalse(user.is_active)
 
 
-    def test_login_success(self):
-        username = "TestUser"
-        password = "Test123$"
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            email=self.user_mail,
+class LoginTests(APITestCase):
+    def setUp(self):
+        self.email_active = "user@example.com"
+        self.username_active = "ActiveTestUser"
+        self.password_active = "Test123$"
+        self.user_active = User.objects.create_user(
+            username=self.username_active,
+            password=self.password_active,
+            email=self.email_active,
             is_active=True
         )
 
-        response = self.client.post(self.login_url, {'email': self.user_mail, 'password': password}, format='json')
+        self.email_not_active = "user@example.com"
+        self.username_not_active = "ActiveTestUser"
+        self.password_not_active = "Test123$"
+        self.user_not_active = User.objects.create_user(
+            username=self.username_not_active,
+            password=self.password_not_active,
+            email=self.email_not_active,
+            is_active=False
+        )
+
+        self.url = reverse('login')
+
+
+    def test_post_success(self):
+        data = {
+            'email': self.email_active,
+            'password': self.password_active
+        }
+
+        response = self.client.post(self.url, data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('access_token', response.cookies)
@@ -137,25 +152,10 @@ class AuthTests(APITestCase):
 
 
     def test_post_fails(self):
-        username = "TestUser"
-        password = "Test123$"
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            email=self.user_mail,
-            is_active=True
-        )
-        user_mot_active = User.objects.create_user(
-            username='username',
-            password='password',
-            email='new@mail.de',
-            is_active=False
-        )
-
         cases = [
-            ("wrong_username", {'username': username, 'password': 'wrong_password'}),
-            ("wrong_password", {'username': 'wrong', 'password': password}),
-            ("inactive_user", {'username': 'username', 'password': 'password'})
+            ("wrong_password", {'email': self.email_active, 'password': 'wrong_password'}),
+            ("wrong_email", {'email': 'wrong@wro.ng', 'password': self.password_active}),
+            ("not_active_user", {'email': self.email_not_active, 'password': self.password_not_active})
         ]
 
         for message, data in cases:
