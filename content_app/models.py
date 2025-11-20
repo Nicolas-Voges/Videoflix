8d52de044816bc4c893816ca5f0e42d624afecdc
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-20 23:01:36 +0100

    Feat: Add video model class in content_app.

--- content_app/models.py
+++ content_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    thumbnail_url = models.URLField()
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Title:{self.title}, ID:{self.id}"
