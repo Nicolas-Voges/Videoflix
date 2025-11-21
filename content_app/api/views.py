a5edbc9b409c5c5c7b80e9d4a23980a72cb1d67c
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-21 16:22:52 +0100

    Feat: Add VideoListAPIView class.

---
+++ content_app/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.generics import ListAPIView
+from content_app.models import Video
+from content_app.api.serializers import VideoListSerializer
+
+
+class VideoListAPIView(ListAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoListSerializer
