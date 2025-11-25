d7fcf7bb7b56805ac783dbb6b722de7ac8897605
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-26 00:26:43 +0100

    Feat: Add tasks.py in content_app.

---
+++ content_app/tasks.py
@@ -0,0 +1,19 @@
+from .models import Video
+from .ffmpeg_commands import generate_hls_files
+
+
+def transcode_video(video_id):
+    video = Video.objects.get(id=video_id)
+    video.status = "processing"
+    video.save()
+
+    try:
+        generate_hls_files(video.original_file.path, video.id)
+
+        video.status = "ready"
+        video.save()
+
+    except Exception as error:
+        video.status = "failed"
+        video.save()
+        raise error
