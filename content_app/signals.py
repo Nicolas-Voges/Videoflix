c17e403a212be725a86df373b27f9c8e10fe6742
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-25 23:27:20 +0100

    Fix: Adjust signals.py for django-rq.

--- content_app/signals.py
+++ content_app/signals.py
@@ -1,6 +1,6 @@
+import django_rq
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from .models import Video
 from .tasks import transcode_video
 
@@ -8,5 +8,5 @@ from .tasks import transcode_video
 @receiver(post_save, sender=Video)
 def start_transcoding_job(sender, instance, created, *args, **kwargs):
     if created:
-        # Create queue job to process video (e.g., generate thumbnails, transcode)
-        transcode_video.delay(instance.id)
+        queue = django_rq.get_queue("default")
+        queue.enqueue(transcode_video, instance.id)
