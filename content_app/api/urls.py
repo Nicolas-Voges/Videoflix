128f567cbca45d4f510de4b2344e2960d807a775
Nicolas Voges <nicolas.voges@hotmail.com>
2025-11-26 03:43:55 +0100

    Feat: Add file responsing view function for hsl playlist.

--- content_app/api/urls.py
+++ content_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import VideoListAPIView
+from .views import VideoListAPIView, video_playlist_view
 
 urlpatterns = [
-    path('video/', VideoListAPIView.as_view(), name="video-list")
+    path('video/', VideoListAPIView.as_view(), name="video-list"),
+    path('api/video/<int:movie_id>/<str:resolution>/index.m3u8', video_playlist_view, name='video-playlist'),
 ]
--- content_app/api/views.py
+++ content_app/api/views.py
@@ -1,10 +1,23 @@
+from pathlib import Path
+
+from django.http import FileResponse, Http404
+from django.conf import settings
+
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+
 from content_app.models import Video
 from content_app.api.serializers import VideoListSerializer
-
 
 class VideoListAPIView(ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoListSerializer
     permission_classes = [IsAuthenticated]
+
+
+def video_playlist_view(request, movie_id: int, resolution: str):
+    playlist_path = Path(settings.MEDIA_ROOT) / f"videos/{movie_id}/{resolution}/index.m3u8"
+    if not playlist_path.exists():
+        raise Http404("Playlist not found")
+
+    return FileResponse(open(playlist_path, "rb"), content_type="application/vnd.apple.mpegurl")
