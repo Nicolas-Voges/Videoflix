e7ba3a5e8d6f3e8bb5626d425ba203255a335640
Nicolas Voges <nicolas.voges@hotmail.com>
2025-12-01 14:22:04 +0100

    Feat: Add VideoAdmin class.

--- content_app/admin.py
+++ content_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Video
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("title", "description")
+
+    readonly_fields = ("status",)
